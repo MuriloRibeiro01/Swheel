6b14c4305aeab1dea65683e1cca44da4b35ced48
MuriloRibeiro01 <murilo.ribeiro2709@gmail.com>
2026-03-10 02:49:58 +0000

    chore: Adição de configurações de container. Integração de banco de dados utilizando .env.example.

--- backend/main.py
+++ backend/main.py
@@ -11,3 +11,6 @@ x = 5
 @app.get("/")
 async def root():
     return {"number": x}
+
+# Planos futuros: utilizar essa API para criar testes unitários
+# tanto para questão de estudos, quanto para qualidade do software.
